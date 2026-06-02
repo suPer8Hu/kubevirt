1504e57ebe73f63a63c99d06d8151768ff622120
Changlin Hu <steviehu95@gmail.com>
2026-06-02 18:25:54 -0500

    virt-api: aligned readiness probe and liveness probe
    Signed-off-by: Changlin Hu <steviehu95@gmail.com>

--- pkg/virt-operator/resource/generate/components/deployments.go
+++ pkg/virt-operator/resource/generate/components/deployments.go
@@ -21,7 +21,6 @@ package components
 
 import (
 	"fmt"
-	"path"
 	"strings"
 
 	networkv1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
@@ -356,6 +355,20 @@ func NewApiServerDeployment(config *operatorutil.KubeVirtDeploymentConfig, produ
 			ContainerPort: 8443,
 		},
 	}
+	container.LivenessProbe = &corev1.Probe{
+		ProbeHandler: corev1.ProbeHandler{
+			HTTPGet: &corev1.HTTPGetAction{
+				Scheme: corev1.URISchemeHTTPS,
+				Port: intstr.IntOrString{
+					Type:   intstr.Int,
+					IntVal: 8443,
+				},
+				Path: "/livez",
+			},
+		},
+		InitialDelaySeconds: 15,
+		PeriodSeconds:       10,
+	}
 	container.ReadinessProbe = &corev1.Probe{
 		ProbeHandler: corev1.ProbeHandler{
 			HTTPGet: &corev1.HTTPGetAction{
@@ -364,7 +377,7 @@ func NewApiServerDeployment(config *operatorutil.KubeVirtDeploymentConfig, produ
 					Type:   intstr.Int,
 					IntVal: 8443,
 				},
-				Path: path.Join("/apis/subresources.kubevirt.io", virtv1.SubresourceGroupVersions[0].Version, "healthz"),
+				Path: "/readyz",
 			},
 		},
 		InitialDelaySeconds: 15,
